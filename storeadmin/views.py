9039fb6fd172f2ee23ab91de9ec716494088e62a
hzy <1109007763@qq.com>
2020-09-13 19:46:30 +0800

    修改提交

--- storeadmin/views.py
+++ storeadmin/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django import conf
 import json
 from django.db.models import Q
-from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # from kingadmin import app_setup
 # from kingadmin import form_handle
 # app_setup.kingadmin_auto_discover()
@@ -16,11 +16,12 @@ from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 # print("sites.",site.enabled_admins)
 
 def app_index(request):
-    #enabled_admins =
-    return HttpResponseRedirect('/storeadmin/err/404')
-    # return render(request,'storeadmin/app_index.html', {'site':'site',})
-	
-def err(request,code):
-    #enabled_admins =
+    # enabled_admins =
+    # return HttpResponseRedirect('/storeadmin/err/404')
+    return render(request, 'storeadmin/index.html', {'site': 'site', })
+
+
+def err(request, code):
+    # enabled_admins =
     # print(request,'这个是')
-    return render(request,'err.html',{'code':code})
+    return render(request, 'err.html', {'code': code, 'message': '页面找不到了'})

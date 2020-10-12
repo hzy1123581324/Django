fe9c1c8eca0291249c20b4c6c7ac00d80c1f7eec
hzy <1109007763@qq.com>
2020-10-12 21:31:18 +0800

    新增blockchain模块，store修改表结构

--- store/models.py
+++ store/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 # 多商家数据库
 
+class Clerk(models.Model):
+    '''
+        店员表
+    '''
+    
 
 class Stores(models.Model):
     """
@@ -37,8 +42,42 @@ class Commodity(models.Model):
 
     '''
     store = models.ForeignKey(Stores, on_delete=models.CASCADE)
-    id = models.AutoField(auto_created=True, primary_key=True,
-                          serialize=False, verbose_name='ID')
+    id = models.AutoField(auto_created=True, primary_key=True,serialize=False, verbose_name='ID')
+    name = models.CharField(max_length=50, verbose_name='名称')
+    caption = models.CharField(max_length=100, verbose_name='副标题')
+    sales = models.IntegerField(default=0, verbose_name='销量')
+    comments = models.IntegerField(default=0, verbose_name='评价数')
+    stock = models.IntegerField(default=0, verbose_name='库存')
+    sales = models.IntegerField(default=0, verbose_name='销量')
+    comments = models.IntegerField(default=0, verbose_name='评价数')
+    is_launched = models.BooleanField(default=True, verbose_name='是否上架销售')
+    default_image_url = models.CharField(max_length=200, default='',
+    	null=True, blank=True, verbose_name='默认图片')
+    
+    class Meta:
+        db_table = 'tb_sku'
+        verbose_name = '商品SKU'
+        verbose_name_plural = verbose_name
+    
+    def __str__(self):
+        return '%s: %s' % (self.id, self.name)
+        
+
+class Brand(BaseModel):
+    """
+    品牌
+    """
+    name = models.CharField(max_length=20, verbose_name='名称')
+    logo = models.ImageField(verbose_name='Logo图片')
+    first_letter = models.CharField(max_length=1, verbose_name='品牌首字母')
+
+    class Meta:
+        db_table = 'tb_brand'
+        verbose_name = '品牌'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class CommodityDetails(models.Model):
@@ -79,3 +118,81 @@ class OrderDetails(models.Model):
     '''
 
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    
+    
+    # OrderInfo.PAY_METHODS_ENUM.get('CASH')   -->  1
+    # OrderInfo.PAY_METHODS_ENUM.get('ALIPAY')   -->  2
+    PAY_METHODS_ENUM = {
+        "CASH": 1,    		# 货到付款
+        "ALIPAY": 2			# 阿里支付
+    }
+    
+    ORDER_STATUS_ENUM = {
+        "UNPAID": 1,
+        "UNSEND": 2,
+        "UNRECEIVED": 3,
+        "UNCOMMENT": 4,
+        "FINISHED": 5
+    }
+    
+    PAY_METHOD_CHOICES = (
+        (1, "货到付款"),
+        (2, "支付宝"),
+    )
+    ORDER_STATUS_CHOICES = (
+        (1, "待支付"),
+        (2, "待发货"),
+        (3, "待收货"),
+        (4, "待评价"),
+        (5, "已完成"),
+        (6, "已取消"),
+    )
+    
+    # 主键, 不会生成默认的主键id
+    order_id = models.CharField(max_length=64, primary_key=True, verbose_name="订单号")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="下单用户")
+    address = models.ForeignKey(Address,
+    	on_delete=models.PROTECT, verbose_name="收获地址")
+    total_count = models.IntegerField(default=1, verbose_name="商品总数")
+    total_amount = models.DecimalField(max_digits=10,
+    	decimal_places=2, verbose_name="商品总金额")
+    freight = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="运费")
+    pay_method = models.SmallIntegerField(choices=PAY_METHOD_CHOICES,
+    	default=1, verbose_name="支付方式")
+    status = models.SmallIntegerField(choices=ORDER_STATUS_CHOICES,
+    	default=1, verbose_name="订单状态")
+    
+    class Meta:
+        db_table = "tb_order_info"
+        verbose_name = '订单基本信息'
+        verbose_name_plural = verbose_name
+    
+
+class OrderGoods(BaseModel):
+    """
+    订单商品
+    """
+    SCORE_CHOICES = (
+        (0, '0分'),
+        (1, '20分'),
+        (2, '40分'),
+        (3, '60分'),
+        (4, '80分'),
+        (5, '100分'),
+    )
+
+    order = models.ForeignKey(OrderInfo,
+    	related_name='skus', on_delete=models.CASCADE, verbose_name="订单")
+    sku = models.ForeignKey(SKU, on_delete=models.PROTECT, verbose_name="订单商品")
+    count = models.IntegerField(default=1, verbose_name="数量")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="单价")
+    comment = models.TextField(default="", verbose_name="评价信息")
+    score = models.SmallIntegerField(choices=SCORE_CHOICES,
+    	default=5, verbose_name='满意度评分')
+    is_anonymous = models.BooleanField(default=False, verbose_name='是否匿名评价')
+    is_commented = models.BooleanField(default=False, verbose_name='是否评价了')
+
+    class Meta:
+        db_table = "tb_order_goods"
+        verbose_name = '订单商品'
+        verbose_name_plural = verbose_name
